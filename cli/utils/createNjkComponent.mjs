6e65d53922c9137714a1dbec992d9a26f96189a1
Ko2doo <ko2doodev@gmail>
2023-10-24 01:03:01 +0500

    FIX:24.10.23-01.01 -> небольшой фикс, добавлена проверка для поиска дубликата, если пользователь передал имя при создании компонента и такой компонент уже существует - будет выдана ошибка.

--- cli/utils/createNjkComponent.mjs
+++ cli/utils/createNjkComponent.mjs
@@ -1,65 +1,75 @@
+/* eslint-disable import/order */
 /* eslint-disable n/no-unpublished-import */
+
 // Утилита для создания компонента
 
 'use strict';
 
 import fs from 'fs';
 // не забываем про остальных юзеров, интегрируем утилиту path
-// eslint-disable-next-line import/order
+
 import path from 'path';
 import chalk from 'chalk';
 
 const createComponent = async (name) => {
   try {
     const dataDirectory = './src/views/data/';
-    const componentDirectory = `./src/views/components/${name}/`;
+    const componentSpawnDirectory = `./src/views/components/${name}/`;
+
+    // проверка на дубликаты
+    const exists = fs.existsSync(componentSpawnDirectory);
 
     // передадим в переменную до расширения файла,
     // имя, пришедшее из функции, и подставим его.
-    const files = [`${name}.njk`, `_${name}.scss`];
+    const files = [` ${name}.njk`, `${name}.mjs`, `_${name}.scss`];
     const componentData = [`${name}.json`];
 
     // проверяем на пустышку
     if (name === undefined) {
       console.error(
         chalk.red(
-          `Для создания компонента необходимо использовать один из двух ключей: \n${chalk.blue('-n')} или ${chalk.blue(
-            '--name',
-          )} передать имя после ключа!`,
+          `Ошибка: Для создания компонента необходимо использовать один из двух ключей: \n${chalk.blue(
+            '-n',
+          )} или ${chalk.blue('--name')} передать имя после ключа!`,
         ),
       );
     } else {
-      // тут объявляем что мы собираемся создать директорию с файлами
-      await fs.mkdir(path.normalize(componentDirectory), { recursive: true }, (err) => {
-        if (err) {
-          console.error(chalk.red(err));
-        }
+      // проверяем на дубликат
+      if (exists === true) {
+        console.error(chalk.red(`Ошибка: Компонент ${name} существует! Попробуйте другое имя`));
+      } else {
+        // тут объявляем что мы собираемся создать директорию с файлами
+        await fs.mkdir(path.normalize(componentSpawnDirectory), { recursive: true }, (err) => {
+          if (err) {
+            console.error(chalk.red(err));
+          }
 
-        console.info(chalk.yellow('------ * component * ------'));
-        console.info(chalk.gray(`Каталог компонентов создан.: ${componentDirectory}`));
+          console.info(chalk.yellow('------ * component * ------'));
+          console.info(chalk.gray(`Каталог компонентов создан.: ${componentSpawnDirectory}`));
 
-        // перебираем массив, записываем значения передаваемое фун-ции
-        // в переменные перед объявлением расширения файла и создаём файлы.
-        files.forEach((file) => {
-          fs.open(`${componentDirectory}${path.basename(file)}`, 'w', (err) => {
-            if (err) {
-              console.error(chalk.red(err));
-            }
+          // перебираем массив, записываем значения передаваемое фун-ции
+          // в переменные перед объявлением расширения файла и создаём файлы.
+          files.forEach((file) => {
+            fs.open(`${componentSpawnDirectory}${path.basename(file)}`, 'w', (err) => {
+              if (err) {
+                console.error(chalk.red(err));
+              }
 
-            console.info(chalk.green(`Файлы компонентов созданы: ${file}`)); // в консоли, говорим что всё ок.
+              console.info(chalk.green(`Файлы компонентов созданы: ${file}`)); // в консоли, говорим что всё ок.
+            });
+          });
+
+          componentData.forEach((data) => {
+            fs.open(`${dataDirectory}${path.basename(data)}`, 'w', (err) => {
+              if (err) {
+                console.error(chalk.red(err));
+              }
+
+              console.info(chalk.green(`Файл данных компонента создан: ${dataDirectory}${data}`)); // в консоли, говорим что всё ок.
+            });
           });
         });
-
-        componentData.forEach((data) => {
-          fs.open(`${dataDirectory}${path.basename(data)}`, 'w', (err) => {
-            if (err) {
-              console.error(chalk.red(err));
-            }
-
-            console.info(chalk.green(`Файл данных компонента создан: ${dataDirectory}${data}`)); // в консоли, говорим что всё ок.
-          });
-        });
-      });
+      }
     }
   } catch (error) {
     console.error(error.message);
