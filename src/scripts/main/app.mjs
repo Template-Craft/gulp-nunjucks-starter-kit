0349f0095d79dc6b0dd7ecf492103895116861c3
Ko2doo <ko2doodev@gmail>
2023-10-29 02:45:43 +0500

    UPD:FIX:29.10.23 -> небольшой апдейт и фикс

--- src/scripts/main/app.mjs
+++ src/scripts/main/app.mjs
@@ -3,7 +3,11 @@
 /* eslint-disable import/order */
 /* eslint-disable no-unused-expressions */
 
-import header from 'Components/header/header';
+'use strict';
+
+import { headerRecolor } from 'Components/header/header';
 import { btnCardAnimals } from './modules/buttons.mjs';
+
+headerRecolor();
 
 btnCardAnimals();
--- src/views/components/header/header.mjs
+++ src/views/components/header/header.mjs
@@ -4,15 +4,18 @@
 const getObjHeader = document.querySelector('.header');
 const objHeader = getObjHeader; // переопределяем для удобства;
 
-objHeader.addEventListener('mouseover', (event) => {
-  event.preventDefault;
+const headerRecolor = () => {
+  objHeader.addEventListener('mouseover', (event) => {
+    event.preventDefault;
 
-  objHeader.setAttribute('data-header-state', 'focused');
-});
+    objHeader.setAttribute('data-header-state', 'focused');
+  });
 
-objHeader.addEventListener('mouseout', (event) => {
-  event.preventDefault;
+  objHeader.addEventListener('mouseout', (event) => {
+    event.preventDefault;
 
-  // objHeader.classList.remove('is-clicked');
-  objHeader.removeAttribute('data-header-state');
-});
+    objHeader.removeAttribute('data-header-state');
+  });
+};
+
+export { objHeader, headerRecolor };
