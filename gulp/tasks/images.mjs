f0a29024158d445f1bca017513bf7ed7cb95d9dc
Ko2doo <ko2doodev@gmail>
2024-06-10 19:34:17 +0500

    FIX:10.06.2024:19:32 => фиксы для задач на копирование шрифтов и картинок, больше информации тут: https://github.com/gulpjs/gulp/issues/2803

--- gulp/tasks/images.mjs
+++ gulp/tasks/images.mjs
@@ -10,7 +10,13 @@
 
 export const images = () => {
   return app.gulp
-    .src(app.path.src.images)
+    .src(app.path.src.images, {
+      // -> Данные параметры устраняют поломку картинок в gulp версии 5
+      // страница с описанием ошибки: https://github.com/gulpjs/gulp/issues/2803
+      encoding: false,
+      buffer: true,
+      removeBOM: false,
+    })
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.plugins.browsersync.stream());
 };
