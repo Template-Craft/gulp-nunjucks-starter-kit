2b14212d5c8757ed4fb987b81350557dd8621b09
Ko2doo <ko2doodev@gmail>
2024-06-16 19:34:40 +0500

    UPD:FIX:16.06.2024:19:32 => Выпелен плагин gulp-notify из-за кретических уязвимостей, теперь ошибки выводятся в консоль, наблюдатель при этом продолжает работать и Gulp не падает.

--- gulp/tasks/scripts.mjs
+++ gulp/tasks/scripts.mjs
@@ -15,85 +15,80 @@ import { fileURLToPath } from 'url';
 const __dirname = nodePath.dirname(fileURLToPath(import.meta.url));
 
 export const scripts = () => {
-  return app.gulp
-    .src(app.path.src.scripts)
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: 'Js',
-          sound: false,
-          message: 'Error: <%= error.message %>',
-        }),
-      ),
-    )
-    .pipe(
-      webpackStream({
-        // режимы работы
-        mode: app.isBuild ? 'production' : 'development',
-        devtool: app.isBuild ? false : 'source-map',
+  return (
+    app.gulp
+      .src(app.path.src.scripts)
+      // ловим ошибки, и выводим их в консоль
+      .pipe(app.plugins.plumber())
+      .pipe(
+        webpackStream({
+          // режимы работы
+          mode: app.isBuild ? 'production' : 'development',
+          devtool: app.isBuild ? false : 'source-map',
 
-        // выходящие файлы
-        output: {
-          filename: '[name].bundle.js',
-          chunkFilename: '[name].js',
-          path: nodePath.resolve(__dirname, 'build'),
-        },
+          // выходящие файлы
+          output: {
+            filename: '[name].bundle.js',
+            chunkFilename: '[name].js',
+            path: nodePath.resolve(__dirname, 'build'),
+          },
 
-        // // Оптимизации
-        // optimization: {
-        //   splitChunks: {
-        //     cacheGroups: {
-        //       vendor: {
-        //         test: /node_modules/,
-        //         chunks: 'initial',
-        //         name: 'vendor',
-        //         enforce: true,
-        //       },
-        //     },
-        //   },
-        // },
+          // // Оптимизации
+          // optimization: {
+          //   splitChunks: {
+          //     cacheGroups: {
+          //       vendor: {
+          //         test: /node_modules/,
+          //         chunks: 'initial',
+          //         name: 'vendor',
+          //         enforce: true,
+          //       },
+          //     },
+          //   },
+          // },
 
-        module: {
-          rules: [
-            {
-              test: /\.m?js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env'],
+          module: {
+            rules: [
+              {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                  },
                 },
               },
-            },
-          ],
-        },
-
-        // скрипты компонент
-        resolve: {
-          alias: {
-            /* Теперь вместо использования относительных путей при импорте:
-             *   # -> import Components from '../../components/components_name';
-             *   # используется псевдоним:
-             *   # -> import component from 'Components/component_folder_name/component_name';
-             */
-
-            Module: nodePath.resolve(__dirname, '/node_modules/'),
-            Components: nodePath.resolve(__dirname, '/src/views/components/'),
+            ],
           },
-          extensions: ['', '.js', '.mjs', '.cjs'],
-        },
-      }),
-      webpack,
-    )
-    .pipe(
-      app.plugins.gulpIf(
-        app.isBuild,
-        app.plugins.rename({
-          suffix: '.min',
+
+          // скрипты компонент
+          resolve: {
+            alias: {
+              /* Теперь вместо использования относительных путей при импорте:
+               *   # -> import Components from '../../components/components_name';
+               *   # используется псевдоним:
+               *   # -> import component from 'Components/component_folder_name/component_name';
+               */
+
+              Module: nodePath.resolve(__dirname, '/node_modules/'),
+              Components: nodePath.resolve(__dirname, '/src/views/components/'),
+            },
+            extensions: ['', '.js', '.mjs', '.cjs'],
+          },
         }),
-      ),
-    )
-    .pipe(app.plugins.plumber.stop())
-    .pipe(app.gulp.dest(app.path.build.scripts))
-    .pipe(app.plugins.browsersync.stream());
+        webpack,
+      )
+      .pipe(
+        app.plugins.gulpIf(
+          app.isBuild,
+          app.plugins.rename({
+            suffix: '.min',
+          }),
+        ),
+      )
+      .pipe(app.plugins.plumber.stop())
+      .pipe(app.gulp.dest(app.path.build.scripts))
+      .pipe(app.plugins.browsersync.stream())
+  );
 };
