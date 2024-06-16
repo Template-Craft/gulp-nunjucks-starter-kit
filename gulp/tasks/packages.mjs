8a7512fb4b6ce89e01240bf24881ea9bd62db7e0
Ko2doo <ko2doodev@gmail>
2024-06-16 18:52:40 +0500

    UPD:FIX:16.06.2024:18:51 => изменил задачу отвечающую за перенос плагинов в проект, удалил лишние задачи в дир-рии gulp/tasks

--- gulp/tasks/packages.mjs
+++ gulp/tasks/packages.mjs
@@ -10,16 +10,11 @@ export const vendors = async () => {
   const modules = app.path.nodeModules;
   const destination = app.path.build.plugins;
 
-  function _getPluginName(name) {
-    return `${modules}/${name}/**/*.*`;
-  }
-
-  function _moveToPluginName(name) {
-    return `${destination}/${name}/`;
-  }
-
+  // Указываем в массиве модули которые хотим подключить в проект, использование:
+  // В массиве указываем путь до модуля (node_modules) и путь до дир-рии в которую Gulp.js их положит:
+  // ['pathToNodeModules/pluginName/pluginSources/**', 'pathToProdDir/pluginName/'],
   const pluginsCollection = await new Map([
-    [`${_getPluginName('normalize.css')}`, `${_moveToPluginName('normalize.css')}`],
+    [`${modules}/normalize.css/normalize.css`, `${destination}/normalize.css/`],
   ]);
 
   await pluginsCollection.forEach((value, key) => {
