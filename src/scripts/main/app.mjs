765c60f761c8dcd6c97cf932bc8f736be8665c18
Ko2doo <ko2doodev@gmail.com>
2024-08-27 13:55:23 +0500

    UPD:FIX: 27.08.2024 временно закомментировал строки.

--- src/scripts/main/app.mjs
+++ src/scripts/main/app.mjs
@@ -1,8 +1,8 @@
 'use strict';
 
-import { headerRecolor } from 'Components/header/header';
+// import { headerRecolor } from 'Components/header/header';
 import { btnCardAnimals } from './modules/buttons.mjs';
 
-headerRecolor();
+// headerRecolor();
 
 btnCardAnimals();
