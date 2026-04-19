9513a00710fe80576bc5e13c59b5dc05f0ed5054
Ko2doo <galaxyrobotix@gmail.com>
2026-04-19 20:24:26 +0500

    RELEASE:2026-04-19:: CLI помощник теперь отдельный пакет на npm (читай README.md). Удалены неиспользуемые зависимости. Обновлено описание.

--- cli-app/commands/index.mjs
+++
@@ -1,9 +0,0 @@
-// Экспортируем все команды
-
-import * as createComponent from './createComponent.mjs';
-import * as injectComponentStyle from './injectComponentStyle.mjs';
-import * as createArchive from './createArchive.mjs';
-import * as base64Converter from './base64Converter.mjs';
-import * as imagemin from './imagemin.mjs';
-
-export const COMMANDS = [createComponent, injectComponentStyle, createArchive, base64Converter, imagemin];
